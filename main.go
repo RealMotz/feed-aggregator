c745ff8477076d6a09fb035a767ffac598d5a3ce
Carlos Motta <carlos.motta.business@gmail.com>
2024-04-26 14:23:46 +0900

    Add feed worker to process feeds concurrently.

--- main.go
+++ main.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"context"
 	"database/sql"
+	"encoding/xml"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
+	"sync"
+	"time"
 
 	"github.com/RealMotz/feed-aggregator/internal/database"
 	"github.com/joho/godotenv"
@@ -43,6 +47,8 @@ func main() {
 		Handler: corsMux,
 	}
 
+	go createInterval(10, cfg.ProcessOldestNFeedsFromDB)
+
 	mux.HandleFunc("GET /v1/readiness", readinessHandler)
 	mux.HandleFunc("GET /v1/err", errorHandler)
 
@@ -60,4 +66,69 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+}
+
+func createInterval(seconds int, fn func(int32)) {
+	ticker := time.NewTicker(time.Duration(seconds) * time.Second)
+	quit := make(chan struct{})
+
+	for {
+		select {
+		case <-ticker.C:
+			fn(10)
+		case <-quit:
+			ticker.Stop()
+			return
+		}
+	}
+}
+
+func (cfg *apiConfig) ProcessOldestNFeedsFromDB(n int32) {
+	ctx := context.Background()
+	feeds, err := cfg.DB.GetNextFeedsToFetch(ctx, n)
+	if err != nil {
+		fmt.Println("error fetching feed")
+		return
+	}
+
+	var group sync.WaitGroup
+
+	for _, feed := range feeds {
+		group.Add(1)
+		go func(url string) {
+			defer group.Done()
+			fetchDataFromFeed(url)
+		}(feed.Url)
+	}
+
+	group.Wait()
+}
+
+func fetchDataFromFeed(feedURL string) {
+	resp, err := http.Get(feedURL)
+	if err != nil {
+		fmt.Println("error fetching feed")
+		return
+	}
+	defer resp.Body.Close()
+
+	type itemList struct {
+		Title           string `xml:"title"`
+		Link            string `xml:"link"`
+		PublicationDate string `xml:"pubDate"`
+		Description     string `xml:"description"`
+	}
+	type xmlEntry struct {
+		ItemList []itemList `xml:"item"`
+	}
+	type xmlData struct {
+		Channel xmlEntry `xml:"channel"`
+	}
+
+	data := xmlData{}
+	decoder := xml.NewDecoder(resp.Body)
+	decoder.Decode(&data)
+
+	fmt.Printf("processing %s\n", feedURL)
+	fmt.Println(len(data.Channel.ItemList))
 }
